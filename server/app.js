68dbc48c950358af3f88a03857308a38be841318
Ritu Raj <rituraj22in@gmail.com>
2018-10-29 12:10:07 +0530

    Added verification for user and student

--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mysql = require('mysql');
 
 const app = express();
 
@@ -17,13 +18,71 @@ app.post('/admin/login', (req, res)=>{
 	var user_id = req.body.admin_id;
 	var password = req.body.password;
 	console.log(user_id, password);
-	res.send('hello baby');
+	con.query("SELECT * FROM users WHERE user_id="+mysql.escape(user_id)+" AND password="+mysql.escape(password), function (err, result, fields) {
+		if(err)
+			throw err;
+		console.log(result);
+		if(typeof result=='undefined'|| result.length==0) {
+			console.log('error');
+			res.status(500).send('Invalid credentials')
+		}
+		else {
+			console.log(result[0].type);
+			if(result[0].type==1)
+				res.send("Done");
+			else
+				res.status(500).send('Given credentials are not of admin')
+
+		}
+	});
+});
+
+app.post('/user/login', (req, res)=>{
+	var user_id = req.body.user_id;
+	var password = req.body.password;
+	console.log(user_id, password);
+	con.query("SELECT * FROM users WHERE user_id="+mysql.escape(user_id)+" AND password="+mysql.escape(password), function (err, result, fields) {
+		if(err)
+			throw err;
+		console.log(result);
+		if(typeof result=='undefined'|| result.length==0) {
+			console.log('error');
+			res.status(500).send('Invalid credentials')
+		}
+		else {
+			console.log(result[0].type);
+			if(result[0].type==2)
+				res.send("Done");
+			else
+				res.status(500).send('Given credentials are not of user')
+
+		}
+	});
 });
 
 app.get('/admin/dashboard', (req, res)=>{
+	console.log(req.body.user_id)
 	res.render('admin_dashboard.ejs', {title: "Admin"});
+});
+
+app.get('/user/dashboard', (req, res)=>{
+	console.log(req.body.user_id)
+	res.render('user_dashboard.ejs', {title: "User"});
 });
 
 app.listen(3000, ()=>{
 	console.log('server is running');
+});
+
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "root",
+  database: "library"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
 });
